a0aa7bb96cf6b9d313b0e5657fb977a0bb1d9e38
John Judge <john@kjef.net>
2022-12-24 14:03:57 +1100

    Processing row and column looks okay

--- test2.py
+++ test2.py
@@ -1,10 +1,14 @@
 import sys
+import fileinput
 
 # some special characters for printing
 B = "|"
 DB = "\u2016"
 HB = "\u2015"
 DHB = "\u2550"
+
+# Usage
+usage = "Usage: " + sys.argv[0] + " [filename]"
 
 a = []
 
@@ -34,11 +38,12 @@ def pprint(s):
             if len(s[i]) != 1:
                 print("x ", end="")
             else:
-                print(f"{s[i]} ", end="")
+                print(f"{s[i][0]} ", end="")
         
         print(DB)
 
     print(DHB * 37)
+
 
 def parseRow(r):
     if len(r) != 9:
@@ -66,6 +71,7 @@ def parseRow(r):
     # row parse, return output
     return o
 
+
 def starting():
     print(f"Enter each row of the starting point")
     print("Use a '-' character to indicate an empty square")
@@ -92,6 +98,95 @@ def starting():
     # have all rows
     return s
 
-a = init()
-#a = starting()
+
+def parseFile():
+    a = []
+    for line in fileinput.input():
+        row = parseRow(line.strip())
+        if len(row) != 9:
+            print(f"Error, could not parse line {line.strip()}", file=sys.stderr)
+            sys.exit(1)
+        
+        for x in range(9):
+            a.append(row[x])
+
+    if len(a) != 81:
+        print("Error parsing file", file=sys.stderr)
+
+    return a
+
+
+def processRow(x, a):
+    # if already a single value then nothing to do
+    if len(a[x]) == 1:
+        return a
+
+    # find start of row, end is start + 9
+    row = (x // 9) * 9
+    print(f"Processing {a[row:row+9]}")
+    for i in range(row, row+9):
+        if i == x:
+            continue
+        if len(a[i]) == 1:
+            c = a[i][0]
+            # remove from a[x]
+            if c in a[x]:
+                a[x].remove(c)
+
+    print(f"Got        {a[row:row+9]}")
+    return a
+
+
+def processSquare(i, a):
+    return a
+
+
+def processColumn(x, a):
+    # if already a single value then nothing to do
+    if len(a[x]) == 1:
+        return a
+
+    # find start of column, subsequent values
+    # are col+9, end is col+(9*8)
+    col = x % 9
+    #print(f"Processing {a[row:row+9]}")
+    for i in range(col, col+72+1, 9):
+        print(f"Index {i}")
+        if i == x:
+            continue
+        if len(a[i]) == 1:
+            c = a[i][0]
+            # remove from a[x]
+            if c in a[x]:
+                a[x].remove(c)
+
+    #print(f"Got        {a[row:row+9]}")
+    return a
+
+
+
+# start the program
+if len(sys.argv) == 1:
+    # get start from user input
+    a = starting
+
+if len(sys.argv) == 2:
+    # treat command line argument as file with starting data
+    a = parseFile()
+
+if len(sys.argv) > 2:
+    print(usage, file=sys.stderr)
+    sys.exit(1)
+
+# have starting point
 pprint(a)
+
+# now the work!
+
+for i in range(81):
+    b = processRow(i, a)
+    c = processSquare(i, b)
+    d = processColumn(i, c)
+    a = d
+
+pprint(d)
